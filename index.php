cca6df3cbe52467f7a6aaf84787e3f08c6ee3671
JackDarkWeb <jacknouatin@yahoo.fr>
2018-11-09 23:39:55 +0200

    put in git

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My projects</title>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		
+		 <!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="style.css"/>
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+           
+			<div id="menu">
+                <span><a href="">Home</a></span>
+
+                <span><a href="">Source codes</a></span>
+					<ul>
+						<li><a href="https://github.com/JackDarkWeb/jackPortfolio">My portfolio</a></li>
+						<li><a href="https://github.com/JackDarkWeb/url-shortener">Shortener</a></li>
+						<li><a href="">RoyalNews</a></li>
+						<li><a href="">Babor</a></li>
+						<li><a href="">Facture Service</a></li>
+						<li><a href="">Waba</a></li>
+					</ul>
+            </div>
+
+
+            <div class="content">
+                <div class="m-b-md">
+                    My projects
+                </div>
+
+                <div class="links">
+                    <a href="http://myprojets.site/portfolio">My portfolio</a>
+                    <a href="http://myprojets.site/shortener">Shortener</a>
+                    <a href="http://myprojets.site/royalnews">RoyalNews</a>
+                    <a href="http://myprojets.site/babor">Babor</a>
+                    <a href="http://myprojets.site/factureflashservice">Facture Flash Service</a>
+                    <a href="http://myprojets.site/waba">Waba</a>
+                </div>
+            </div>
+			
+        </div>
+		
+		<script src="jquery_code.js"></script>
+    </body>
+</html>
---
+++ jquery_code.js
@@ -0,0 +1,53 @@
+$(function(){
+
+    $('#menu > span a').click(function (e) {
+        e.preventDefault();
+    })
+
+    $(window).click(function () {
+        $('body, html').toggleClass('new_font');
+    });
+
+    div = $('#menu > span:eq(1)');
+
+    $('#menu ul > li').hide();
+
+    $(window).click(function(e) {
+
+        // Si ce n'est pas div ni un de ses enfants
+        if(!$(e.target).is(div) && !$.contains(div[0],e.target) ) {
+            $('#menu ul > li').slideUp('slow').fadeOut('slow');
+        }
+    });
+
+    $(div).click(function(){
+
+        $('#menu ul > li').slideDown('slow').fadeIn('slow').hover(
+
+            function () {
+                $(this).addClass('addClass a').stop().animate({
+                        paddingLeft: '30px',
+                    }, 400, 'linear',
+                    function () {
+                        $(this).css('background', 'white');
+                    }
+                );
+            },
+            function () {
+                $(this).removeClass('addClass a').stop().animate({
+                        paddingLeft: '0px',
+                    }, 300, 'linear',
+                    function () {
+                        $(this).css('background', 'cornsilk');
+                    }
+                );
+            }
+        );
+
+
+    });
+
+
+
+
+});
